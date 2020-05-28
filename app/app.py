372d70395b1f12d7acb4ba89c778341c70273d1f
akshat113 <akshat113@yahoo.co.in>
2020-05-28 12:54:07 -0500

    final

--- app/app.py
+++ app/app.py
@@ -205,24 +205,6 @@ code_station_abbrev = dict(map(reversed, station_code_abbrev.items()))
 def hello():
     return render_template('index.html')
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 @app.route('/get', methods=['GET'])
 def api_all():
     if 'origin_station_id' in request.args:
